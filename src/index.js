64628c7abf182a869d67b9a646345ee952ee4972
Alex <robert.perez6@g.austincc.edu>
2019-12-12 11:42:26 -0600

    We did 5 and 6

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import 'normalize.css';
 import './index.css';
 
 import api from './api';
+import store from './store';
 
 import shoppingList from './shopping-list';
 
@@ -13,6 +14,12 @@ const main = function () {
 
   // eslint-disable-next-line indent
     //.then(res => console.log(res));
+    api.getItems()
+    .then(res => res.json())
+    .then((items) => {
+      items.forEach((item) => store.addItem(item));
+      shoppingList.render();
+    });
   
 
   shoppingList.bindEventListeners();
